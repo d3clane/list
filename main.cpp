b3fc2bd8c562146a61ef275ec6fd918f0d527451
Arman <ovakimian.al@phystech.edu>
2023-11-03 19:08:49 +0300

    cleaned

--- main.cpp
+++ main.cpp
@@ -12,44 +12,29 @@ int main(const int argc, const char* argv[])
 
     size_t lastPos = 0;
 
-    //LIST_TEXT_DUMP(&list);
-    //printf("HERE1\n");
-    //printf("next free blockAA: %d\n", list.data[list.freeBlockHead].nextPos);
+    ListInsert(&list, list.end, 78, &lastPos);
 
-    ListInsert(&list, LIST_END, 78, &lastPos);
-
-    //printf("lastPos: %d\n", lastPos);
-    //printf("next free blockAB: %d\n", list.data[list.freeBlockHead].nextPos);
     size_t newLastPos = 0;
     ListInsert(&list, lastPos, 123, &newLastPos);
     
-    //printf("lastPos: %d\n", lastPos);
-
     LIST_DUMP(&list);
     
     ListInsert(&list, lastPos, 1488, &newLastPos);
 
-    //printf("lastPos: %d\n", lastPos);
     LIST_DUMP(&list);
 
     ListErase(&list, newLastPos);
     ListInsert(&list, lastPos, 1111, &lastPos);
 
-    //printf("lastPos: %d\n", lastPos);
-
-    ListInsert(&list, LIST_END, 9999, &newLastPos);
-
-    //printf("lastPos: %d\n", lastPos);
+    ListInsert(&list, list.end, 9999, &newLastPos);
 
     LIST_DUMP(&list);
 
     ListInsert(&list, lastPos, 123, &lastPos);
 
-    //printf("lastPos: %d\n", lastPos);
-
     ListErase(&list, newLastPos);
 
-    ListRebuild(&list);
+    //ListRebuild(&list);
     LIST_DUMP(&list);
 
     ListDtor(&list);
